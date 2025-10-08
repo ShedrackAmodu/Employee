8b6965eb5f12772735179e60189c5ec01b61fdbf
ShedrackAmodu <147725659+ShedrackAmodu@users.noreply.github.com>
2025-10-08 18:47:31 +0100

    Update settings.py

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -1,11 +1,6 @@
 import os
 from pathlib import Path
 
-"""
-Django settings for employee_management project.
-"""
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
